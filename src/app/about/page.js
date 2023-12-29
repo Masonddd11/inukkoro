cc986f44128986225b49cc6fdfce63f9b0154544
Mason WONG <fpsmason2020@gmail.com>
2023-12-29 19:31:18 +0800

    responsive update

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -23,7 +23,7 @@ function about() {
       </div>
       <div className="">
         <div className="min-w-full bg-white min-h-screen p-[30px] md:p-[100px] border-white dark:bg-[var(--dark-primary)]">
-          <div className="md:hero-content   bg-white text-black dark:bg-[var(--dark-primary)] dark:text-white">
+          <div className="md:hero-content md:flex-col  bg-white text-black dark:bg-[var(--dark-primary)] dark:text-white">
             <h1 className="text-4xl font-bold font-roboto">AIM WITH BARK</h1>
             <br />
             <p className="text-3xl bold">
